a29f9408bf1083576f6b325fec089db650fb9278
Matías <matiasarce214@gmail.com>
2023-06-05 19:22:10 -0300

    verificando ruta view

--- Desafio-Complementario/dao/routes/view.router.js
+++ Desafio-Complementario/dao/routes/view.router.js
@@ -18,20 +18,4 @@ viewRouter.use('/', async (req, res) => {
 
 });
 
-/*viewRouter.post('/', async (req,res) => {
-
-    const msn = req.body;
-    
-    try {
-        
-        const messageAdded = await messagesService.addMessage(msn);
-        res.send(messageAdded);
-
-    } catch (error) {
-        
-        res.status(500).send(`${error}`);
-
-    }
-})*/
-
 export default viewRouter;
