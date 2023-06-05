676e38b34a78dc7ce0309e84373247ad42b13b3f
Matías <matiasarce214@gmail.com>
2023-06-04 23:25:02 -0300

    agregando views

--- Desafio-Complementario/dao/app.js
+++ Desafio-Complementario/dao/app.js
@@ -2,22 +2,42 @@ import express from 'express';
 import handlebars from 'express-handlebars';
 import mongoose from 'mongoose';
 import productRouter from './routes/product.router.js';
+import viewRouter from './routes/view.router.js';
+import { Server } from 'socket.io';
 
 const app = express();
+const messages = [];
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', handlebars.engine());
 app.set('views', 'views/');
 app.set('view engine', 'handlebars');
 
 app.use('/api/products', productRouter);
+app.use('/', viewRouter);
+//app.use(express.static('public'));
 
 mongoose.connect('mongodb+srv://matiasarce214:123@ecommerce.lmtrfb7.mongodb.net/?retryWrites=true&w=majority');
 
-app.listen(8080, () => {
+const webServer = app.listen(8080, () => {
 
     console.log('Escuchando puerto 8080');
 
+});
+
+const io = new Server(webServer);
+
+io.on('connection', (socket) => {
+
+    socket.emit('messages', messages);
+
+    socket.on('message', (message) => {
+
+        console.log(message);
+        messages.push(message);
+
+        io.emit('messages', messages);
+    });
 });
---
+++ Desafio-Complementario/dao/models/messages.model.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+export const messageSchema = new mongoose.Schema({
+
+    user: String,
+    message: String
+
+});
+
+export const messageModel = mongoose.model('messages', messageSchema);
