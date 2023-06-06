9edfd6ca8d5bd07015089707ed98672f24177d81
Matías <matiasarce214@gmail.com>
2023-06-06 11:32:54 -0300

    Reinicio

--- Desafio-04/src/utils/instancias.js
+++
@@ -1,5 +0,0 @@
-import ProductManager from "../controller/ProductManager.js";
-import CartsManager from "../controller/CartsManager.js";
-
-export const listadoProductos = new ProductManager();
-export const listadoCarrito = new CartsManager();
