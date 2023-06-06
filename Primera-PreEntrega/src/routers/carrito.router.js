6e50d819940b649bcb32b6a3053ff7e0237ef60e
Matías <matiasarce214@gmail.com>
2023-06-06 10:35:11 -0300

    Corrección en CartRouter

--- Primera-PreEntrega/src/routers/carrito.router.js
+++ Primera-PreEntrega/src/routers/carrito.router.js
@@ -8,7 +8,7 @@ carritoRouter.post('/', async (req, res) => {
 
     try {
 
-        
+
         res.send(await c.agregarCarrito());
 
     } catch (error) {
@@ -18,14 +18,14 @@ carritoRouter.post('/', async (req, res) => {
     }
 });
 
-carritoRouter.get('/', async (req,res) => {
+carritoRouter.get('/', async (req, res) => {
 
     try {
-        
+
         res.send(await c.leerCarrito());
 
     } catch (error) {
-        
+
         res.status(400).send(`${error}`);
 
     }
@@ -34,27 +34,27 @@ carritoRouter.get('/', async (req,res) => {
 carritoRouter.get('/:cid', async (req, res) => {
 
     try {
-        
-        let id = req.params.cid;
-        res.send(await c.carritoPorId(id));
-        
+
+        //let id = req.params.cid;
+        res.send(await c.carritoPorId(parseInt(req.params.cid)));
+
     } catch (error) {
-        
+
         res.status(400).send(`${error}`)
     }
 });
 
-carritoRouter.post('/:cid/product/:pid', async (req,res) => {
+carritoRouter.post('/:cid/product/:pid', async (req, res) => {
 
     try {
-        
+
         let cartId = req.params.cid;
         let productId = req.params.pid;
 
-        res.send(await c.agregarProductoEnCarrito(cartId, productId));
-        
+        res.send(await c.agregarProductoEnCarrito(parseInt(cartId), parseInt(productId)));
+
     } catch (error) {
-        
+
         res.status(400).send(`${error}`);
 
     }
